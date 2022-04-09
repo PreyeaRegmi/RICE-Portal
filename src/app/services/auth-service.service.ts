d73cdab5227d68bb2e38fe8de0806d75f7a7c371
Preyea Regmi <preyearegmi@outlook.com>
2022-04-09 12:44:04 -0500

    dev : module route fixes

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -9,13 +9,14 @@ export class AuthService implements CanActivate {
   constructor(public router: Router) { }
 
   canActivate(): boolean {
-    let auth = localStorage.getItem("auth");
-    if (auth != null && auth.length > 0)
-      return true;
-    else {
-      this.router.navigate(['login']);
-      return false;
-    }
+    return true;
+    // let auth = localStorage.getItem("auth");
+    // if (auth != null && auth.length > 0)
+    //   return true;
+    // else {
+    //   this.router.navigate(['login']);
+    //   return false;
+    // }
 
   }
 
