caef02764717f481212f98f834036a396d3ed018
Preyea Regmi <preyearegmi@outlook.com>
2022-04-14 20:52:08 -0500

    dev : default images added

--- src/app/home/expimagelisting/expimagelisting.component.ts
+++ src/app/home/expimagelisting/expimagelisting.component.ts
@@ -24,6 +24,22 @@ export class ExpimagelistingComponent implements OnInit {
 
     this.imageService.getImages().subscribe(imgList => {
       this.imageList = imgList;
+      if(this.imageList.length<1)
+      {
+        this.imageList = [
+          "https://picsum.photos/200/301",
+          "https://picsum.photos/200/302",
+          "https://picsum.photos/200/303",
+          "https://picsum.photos/200/304",
+          "https://picsum.photos/200/305",
+          "https://picsum.photos/200/306",
+          "https://picsum.photos/200/307",
+          "https://picsum.photos/200/308",
+          "https://picsum.photos/200/309",
+          "https://picsum.photos/200/310",
+          "https://picsum.photos/200/311",
+        ];
+      }
     },
       error => {
         this.imageList = [
